1476385ac892d18c0a1748efc4e0f7a0fb178a02
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-16 15:57:21 -0500

    Making optional the findByUsername method response

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,10 +1,11 @@
 package wolox.training.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  User findByUsername(String username);
+  Optional<User> findByUsername(String username);
 
 }
