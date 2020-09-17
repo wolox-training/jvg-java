36ddb5c84fb36d16e0a89f3e28051720053c175c
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-17 12:08:04 -0500

    HttpStatus for BookIdMismatchException modified for an appropiated one

--- src/main/java/wolox/training/exception/BookIdMismatchException.java
+++ src/main/java/wolox/training/exception/BookIdMismatchException.java
@@ -3,7 +3,7 @@ package wolox.training.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.PRECONDITION_FAILED, reason= "Book Id Mismatch.")
+@ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY, reason= "Book Id Mismatch.")
 public class BookIdMismatchException extends RuntimeException {
 
   public static final String BOOK_ID_DOESNT_MATCH = "Book Id doesn't match. Book ID: %d, Provided ID: %d";
