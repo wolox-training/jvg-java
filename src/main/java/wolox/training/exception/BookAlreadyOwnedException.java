ffcbd209f0a952dd11f80f31694d012e73b343f1
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-17 12:06:14 -0500

    HttpStatus for BookAlreadyOwnedException modified for an appropiated one

--- src/main/java/wolox/training/exception/BookAlreadyOwnedException.java
+++ src/main/java/wolox/training/exception/BookAlreadyOwnedException.java
@@ -3,7 +3,7 @@ package wolox.training.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.OK, reason= "Book Already Owned.")
+@ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY, reason= "Book Already Owned.")
 public class BookAlreadyOwnedException extends RuntimeException{
 
   public static final String BOOK_ALREADY_OWNED = "Book already owned. Book Title: %s.";
