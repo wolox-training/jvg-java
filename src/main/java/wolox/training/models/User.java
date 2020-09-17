025a708ccc25985f1930192dc1df6b4622e36d21
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-17 12:25:54 -0500

    Fixing deleteBook method

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -5,13 +5,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import wolox.training.exception.BookAlreadyOwnedException;
@@ -84,6 +83,8 @@ public class User {
   }
 
   public void deleteBook(Book book){
-    this.books.remove(book);
+    this.setBooks(this.books.stream()
+        .filter(bookInList -> bookInList.getId() != book.getId())
+        .collect(Collectors.toList()));
   }
 }
