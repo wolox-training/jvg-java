0824cad61b3284c036aebd3810242e2d5452a1ec
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-18 11:11:01 -0500

    Fixing pathVariable name to match the method argument name

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -34,7 +34,7 @@ public class BookController {
     return bookRepository.findAll();
   }
 
-  @GetMapping("/author/{bookAuthor}")
+  @GetMapping("/author/{author}")
   public Book findByAuthor(@PathVariable String author){
     return bookRepository.findByAuthor(author)
         .orElseThrow(() -> new BookNotFoundException(author));
