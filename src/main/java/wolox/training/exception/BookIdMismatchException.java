f86919577d538705b079d636b47a8130db604b2e
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-21 16:40:54 -0500

    ExceptionConstants class added

--- src/main/java/wolox/training/exception/BookIdMismatchException.java
+++ src/main/java/wolox/training/exception/BookIdMismatchException.java
@@ -1,9 +1,9 @@
 package wolox.training.exception;
 
 
-public class BookIdMismatchException extends RuntimeException {
+import static wolox.training.constants.ExceptionConstants.BOOK_ID_DOESNT_MATCH;
 
-  public static final String BOOK_ID_DOESNT_MATCH = "Book Id doesn't match. Book ID: %d, Provided ID: %d";
+public class BookIdMismatchException extends RuntimeException {
 
   public BookIdMismatchException(Long bookId, Long id){
     super(String.format(BOOK_ID_DOESNT_MATCH, bookId, id));
