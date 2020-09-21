8cea1c15980b782ffd76fc57ef8328fbcf14bf47
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-21 11:42:23 -0500

    Adding BookAlreadyOwnedException to the CustomExceptionHandler

--- src/main/java/wolox/training/exception/BookAlreadyOwnedException.java
+++ src/main/java/wolox/training/exception/BookAlreadyOwnedException.java
@@ -1,9 +1,5 @@
 package wolox.training.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY, reason= "Book Already Owned.")
 public class BookAlreadyOwnedException extends RuntimeException{
 
   public static final String BOOK_ALREADY_OWNED = "Book already owned. Book Title: %s.";
