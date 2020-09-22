10c692f9b120a564e93eb893767bab2bb323bc9e
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-22 15:39:02 -0500

    adding unique param to username - user model

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -32,7 +32,7 @@ public class User {
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name="id")
   private long userId;
-  @Column(nullable = false)
+  @Column(nullable = false, unique = true)
   private String username;
   @Column(nullable = false)
   private String name;
