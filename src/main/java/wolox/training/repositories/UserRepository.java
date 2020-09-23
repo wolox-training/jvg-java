0cc4a075ee0d02b749f99105201687bc75f7d91c
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-23 13:01:18 -0500

    setting custom queries to repositories

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -3,12 +3,21 @@ package wolox.training.repositories;
 import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import wolox.training.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByUsername(String username);
 
-  Iterable<User> findAllByBirthdateBetweenAndNameContainingIgnoreCase(LocalDate startDate, LocalDate endDate, String name);
+  @Query("SELECT u FROM User u"
+      + "WHERE (:startDate is null OR u.birthdate >= :startDate)"
+      + "AND (:endDate is null OR u.birthdate <= :endDate)"
+      + "AND (:name is null OR UPPER(u.name) LIKE UPPER(CONCAT('%',:name,'%')))")
+  Iterable<User> findAllByBirthdateBetweenAndNameContainingIgnoreCase(
+      @Param("startDate") LocalDate startDate,
+      @Param("endDate") LocalDate endDate,
+      @Param("name") String name);
 
 }
