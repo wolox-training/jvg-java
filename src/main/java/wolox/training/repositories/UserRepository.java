2732abb30f6c9b131684c0bedb86e595ae2f46bc
julianvasquez-wolox <julian.vasquez@wolox.co>
2020-09-23 15:34:38 -0500

    Fixing userRepository custom query

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -14,7 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
   @Query("SELECT u FROM User u"
       + " WHERE (cast(:startDate as date) is null OR u.birthdate >= :startDate)"
       + " AND (cast(:endDate as date) is null OR u.birthdate <= :endDate)"
-      + " AND (:name is null OR UPPER(u.name) LIKE UPPER(CONCAT('%',:name,'%')))")
+      + " AND (:name = '' OR UPPER(u.name) LIKE UPPER(CONCAT('%',:name,'%')))")
   Iterable<User> findAllByBirthdateBetweenAndNameContainingIgnoreCase(
       @Param("startDate") LocalDate startDate,
       @Param("endDate") LocalDate endDate,
